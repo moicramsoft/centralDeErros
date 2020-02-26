f887627722c8bce1b1911657673c7eda45b17e34
MoicraSilva <moicramsilva@gmail.com>
2020-02-25 23:47:59 -0300

    Central de Erros V1

---
+++ src/main/java/com/central/de/erros/codenation/CodenationApplication.java
@@ -0,0 +1,13 @@
+package com.central.de.erros.codenation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodenationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodenationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/central/de/erros/codenation/CodenationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.central.de.erros.codenation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodenationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
